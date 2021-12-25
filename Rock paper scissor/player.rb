3421d210b09937653974aa8d76eba5d155db9e5e
tarun-29 <29kantiwaltarun@gmail.com>
2021-12-25 11:00:52 +0530

    some extra code of rock paper scissors game

---
+++ Rock paper scissor/player.rb
@@ -0,0 +1,31 @@
+class Player
+  MOVES = [:rock, :paper, :scissors]
+
+  attr_reader :score, :move, :name
+
+  def initialize(name)
+    @name = name
+    @score = 0
+    @move = nil
+  end
+
+  def get_move
+    loop do
+      puts "Pick a move"
+      print "> "
+      @move = gets.chomp.strip.downcase.to_sym
+      if @move == :quit
+        return false
+      elsif MOVES.include?(@move)
+        return @move
+      else
+        puts "Invalid Move"
+      end
+    end
+  end
+
+  def win
+    @score +=1
+  end
+end
+
